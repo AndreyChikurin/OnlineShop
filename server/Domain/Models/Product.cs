09dcba4013b1a282ca0c68723c6a235f7243002d
Andrei Chikurin <andrei.chikurin@akvelon.com>
2021-09-17 15:41:58 +0300

    Added Models and DTO in Entity_Dto branch

--- server/Domain/Models/Product.cs
+++ server/Domain/Models/Product.cs
@@ -1,9 +1,19 @@
 ﻿namespace Domain.Models
 {
+    using System;
+
     public class Product
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 
         public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Img { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Category CategoryType { get; set; }
     }
 }
