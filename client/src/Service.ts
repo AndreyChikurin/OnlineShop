70cf269fe4c0fa76787745b7cf37bc4fa64d17ef
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-14 14:37:29 +0300

    Added a new class and methods for POST PUT DELETE

--- client/src/Service.ts
+++ client/src/Service.ts
@@ -1,7 +1,10 @@
+import { NewProduct } from './models/Product';
+import { API_CATEGORIES, API_PRODUCTS } from './utils/consts';
+
 export class Service {
   async getProducts() {
     try {
-      const response = await fetch('/api/Products');
+      const response = await fetch(API_PRODUCTS);
       const productData = await response.json();
       return productData;
     } catch (error) {
@@ -11,21 +14,67 @@ export class Service {
 
   async getProduct(id: string) {
     try {
-      const response = await fetch('/api/Products/' + id);
+      const response = await fetch(API_PRODUCTS + '/' + id);
       const productData = await response.json();
       return productData;
     } catch (error) {
       throw Error('Failed to fetch 1 product');
     }
   }
-  
+
   async getCategories() {
     try {
-      const ans = await fetch('/api/Categories');
+      const ans = await fetch(API_CATEGORIES);
       const categoryData = await ans.json();
       return categoryData;
     } catch (error) {
       throw Error('Failed to fetch categories');
+    }
+  }
+
+  async saveProduct(product: NewProduct) {
+    try {
+      const reqOptions = {
+        method: 'POST',
+        body: JSON.stringify(product),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const response = await fetch(API_PRODUCTS, reqOptions);
+      const result = await response.json();
+      return result.id;
+    } catch (error) {
+      throw Error('Failed to save the product');
+    }
+  }
+
+  async deleteProduct(id: string) {
+    try {
+      const reqOptions = {
+        method: 'DELETE',
+      };
+      const response = await fetch(API_PRODUCTS + '/' + id, reqOptions);
+      return response;
+    } catch (error) {
+      throw Error('Failed to delete the product');
+    }
+  }
+
+  async putProduct(product: NewProduct) {
+    try {
+      const reqOptions = {
+        method: 'PUT',
+        body: JSON.stringify(product),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const response = await fetch(API_PRODUCTS, reqOptions);
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      throw Error('Failed to put the product');
     }
   }
 }
